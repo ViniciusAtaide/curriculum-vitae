4ba943ba36e5fd7204b5b6d5a3679b1907af8947
Vinicius Ataide <viniciusataid@gmail.com>
2025-08-22 16:40:46 +0000

    fix screenshot bug

--- assets/js/site.js
+++ assets/js/site.js
@@ -1,22 +1,18 @@
 // profile click
 const profile = document.getElementById("profile");
 
-const goTo = () => window.open("https://github.com/cyruzin");
+const goTo = () => window.open("https://github.com/viniciustataide");
 
 profile.addEventListener("click", goTo);
 
 // prepare the page for printing
 const print = document.getElementById("print");
 const sideMenuLeft = document.getElementById("side-menu-left");
-const certifications = document.getElementById("certifications");
-const locationFlag = document.getElementById("location");
 const contact = document.getElementById("contact");
 
 const hideMenu = () => {
   sideMenuLeft.style.display = "none";
-  certifications.style.display = "none";
   print.style.display = "none";
-  locationFlag.style.display = "none";
   contact.style.display = "block";
 
   window.print();
@@ -25,9 +21,7 @@ const hideMenu = () => {
 const showMenu = () => {
   contact.style.display = "none";
   sideMenuLeft.style.display = "block";
-  certifications.style.display = "block";
   print.style.display = "block";
-  locationFlag.style.display = "flex";
 };
 
 print.addEventListener("click", hideMenu);
